a0fd2bc8870c097a286b1fad592d82650a27adae
annaraysk <annaraykalashetty1999@gmail.com>
2020-08-27 17:29:39 +0530

    Antilog for pubg (depracated)
    This file has code which prevents pubg from logging the changes made to game environment.
    This deletes the logs file generated before it gets uploaded to pubg servers.
    Code is outdated, do not try this on present update, can lead to account bans if detected by pubg anti-cheat.

---
+++ antilog.sh
@@ -0,0 +1,104 @@
+cd /data/data/com.tencent.ig && rm -rf app_crashrecord files && echo "Q3JlYXRvciA6IEZhbW91cyBQbGF5ZXIgRXh0ZXJtaW5hdG9yClRlbGVncmFtIDogdC5tZS9MZWFkZXJGUEU=" > files && echo "Q3JlYXRvciA6IEZhbW91cyBQbGF5ZXIgRXh0ZXJtaW5hdG9yClRlbGVncmFtIDogdC5tZS9MZWFkZXJGUEU=" > app_crashrecord
+touch /data/data/com.tencent.ig/cache
+touch /data/data/com.tencent.ig/code_cache
+touch /data/data/com.tencent.ig/app_bugly
+touch /data/data/com.tencent.ig/no_backup
+touch /data/data/com.tencent.ig/app_textures
+touch /data/data/com.tencent.ig/app_webview
+some(){
+	rm -rf /data/data/com.tencent.ig/app_appcache &> /dev/null
+rm -rf /data/data/com.tencent.ig/app_bugly &> /dev/null
+rm -rf /data/data/com.tencent.ig/cache &> /dev/null
+rm -rf /data/data/com.tencent.ig/code_cache &> /dev/null
+rm -rf /data/data/com.tencent.ig/databases &> /dev/null
+rm -rf /data/data/com.tencent.ig/cache
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache
+rm -rf /data/data/com.tencent.ig/app_appcache
+rm -rf /data/data/com.tencent.ig/app_bugly
+rm -rf /data/data/com.tencent.ig/app_databases
+rm -rf /data/data/com.tencent.ig/app_geolocation
+rm -rf /data/data/com.tencent.ig/app_tbs
+rm -rf /data/data/com.tencent.ig/app_textures
+rm -rf /data/data/com.tencent.ig/app_webview
+rm -rf /data/data/com.tencent.ig/app_webview_imsdk_inner_webview
+rm -rf /data/data/com.tencent.ig/cache
+rm -rf /data/data/com.tencent.ig/code_cache
+rm -rf /data/data/com.tencent.ig/databases
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+rm -rf /storage/emulated/0/tencent
+rm -rf /storage/emulated/0/Tencent
+rm -rf /storage/emulated/0/.backups
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/AntiCheat.ini
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/puffer_temp &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/new.filelist &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifsbk &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/afd &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/IGH5Cache &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/ImageDownload &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/rawdata &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Updateinfo &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo/RoleInfo.json &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/GameErrorNoRecords &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo/apollo_uuid_define.json &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo/apollo_loglist.json &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Pandora &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/1375135419_47_0.17.0.11807_20200319093652_1583227858_cures.ifs.res &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp &> /dev/null
+rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/ca-bundle.pem &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt &> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/vmpcloudconfig.json &> /dev/null
+rm -rf /data/data/com.tencent.ig/cache/* &> /dev/null
+rm -rf /sdcard/Tencent/beacon &> /dev/null
+rm -rf /sdcard/Tencent/wtlogin/com.tencent.mobileqq &> /dev/null
+rm -rf /sdcard/Tencent/blob/mqq &> /dev/null
+rm -rf /sdcard/Tencent/Midas/Log/com.tencent.ig &> /dev/null
+rm -rf /sdcard/Tencent/wtlogin/com.tencent.mobileqq &> /dev/null
+rm -rf /sdcard/Tencent/tbs_live_log/com.tencent.mobileqq &> /dev/null
+rm -rf /sdcard/Tencent/tbs_audio_data &> /dev/null
+rm -rf /sdcard/Tencent/tbs/backup/com.tencent.mobileqq &> /dev/null
+rm -rf /sdcard/Tencent/tbs/com.tencent.mobileqq &> /dev/null
+rm -rf /sdcard/Tencent/QQfile_recv/.TbsReaderTempcom.tencent.mobileqq &> /dev/null
+rm -rf /sdcard/Tencent/QQfile_recv/.thumbnails &> /dev/null
+rm -rf /sdcard/Tencent/QQfile_recv/.tmp &> /dev/null
+rm -rf /sdcard/Tencent/QQfile_recv/.trooptmp &> /dev/null
+rm -rf /sdcard/Tencent/msflogs/com/tencent/mobileqq &> /dev/null
+rm -rf /sdcard/Tencent/MobileQQ/.apollo/game &> /dev/null
+rm -rf /sdcard/Tencent/MobileQQ/capture_qsvf &> /dev/null
+rm -rf /sdcard/Tencent/MobileQQ/dov_ptv_template_dov &> /dev/null
+rm -rf /sdcard/Tencent/MobileQQ/WebViewCheck &> /dev/null
+rm -rf /sdcard/Tencent/MobileQQ/log &> /dev/null
+rm -rf /data/data/com.tencent.ig/code_cache/* &> /dev/null
+echo "\nAutomatic Clearlog...."
+sleep 4
+}
+PACKAGE='com.tencent.ig'
+while [ $(pidof $PACKAGE) ]
+do
+cleaner
+if [ ! $(pidof $PACKAGE) ]; then
+break
+fi
+done
+echo "\nStopped Automatic Clearlog"
+sleep 2
+echo "\nDone"
